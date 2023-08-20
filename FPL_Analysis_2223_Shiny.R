56c04bd357e1d951dc0ef6fba2e3c8c32b113c01
tonglam <bluedragon00000@gmail.com>
2023-08-20 13:42:56 +0800

    init radar map

--- FPL_Analysis_2223_Shiny.R
+++ FPL_Analysis_2223_Shiny.R
@@ -1,7 +1,5 @@
-## input: position, club, player_name;  xG, G; xA, A
-
-library(shiny)
 library(tidyverse)
+library(fmsb)
 
 # load data
 path <- "./FPL_Dataset_2022-2023.csv"
@@ -31,9 +29,7 @@ fpl_data <- fpl_raw_data[c(
       cost = "now_cost",
       goals = "goals_scored",
       xG = "expected_goals",
-      xA = "expected_assists",
-      cs = "clean_sheets",
-      xGc = "expected_goals_conceded"
+      xA = "expected_assists"
     )
     
   )) %>%
@@ -41,74 +37,154 @@ fpl_data <- fpl_raw_data[c(
   mutate(cost = cost / 10)
 
 # prepare data for select choices
-positions <-  c("ALL", "FWD", "MID", "DEF", "GKP")
-clubs <- c("ALL", unique(fpl_data$club))
-costs <- c("ALL", seq(from = 4, to = 13.5, by = 0.5))
+positions <-  c("FWD", "MID", "DEF", "GKP")
+clubs <- c(unique(fpl_data$club))
+costs <- c(seq(from = 4, to = 13.5, by = 0.5))
+
+# radar colnames
+radarCols <- c("points",  "goals", "xG", "minutes", "assists", "xA")
 
 # User interface ----
-ui <- fluidPage(titlePanel("FPL_Analysis_2223"),
-                
-                sidebarLayout(
-                  sidebarPanel(
-                    selectInput(
-                      "position",
-                      label = "Position",
-                      choices = positions,
-                      selected = positions[1]
-                    ),
-                    selectInput(
-                      "club",
-                      label = "Club",
-                      choices = clubs,
-                      selected = clubs[1]
-                    ),
-                    selectInput(
-                      "cost",
-                      label = "Cost",
-                      choices = costs,
-                      selected = costs[1]
-                    ),
-                    # player is a reactive selectInput
-                    selectInput("player", label = "Player", choices = "")
-                    
-                    
-                  ),
-                  
-                  mainPanel("aaa")
-                  
-                ))
+ui <- fluidPage(#theme = bslib::bs_theme(bootswatch = "darkly"),
+  
+  titlePanel("FPL_Analysis_2223"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      # Radar chart to compare two players
+      
+      # player1
+      selectInput(
+        "club1",
+        "Player 1 - Club",
+        choices = clubs,
+        selected = clubs[1]
+      ),
+      
+      selectInput(
+        "position1",
+        "Player 1 - Position",
+        choices = positions,
+        selected = positions[1]
+      ),
+      
+      selectInput("player1",
+                  "Player 1 - Name",
+                  choices = NULL),
+      
+      br(),
+      br(),
+      
+      # player2
+      selectInput(
+        "club2",
+        "Player 2 - Club",
+        choices = clubs,
+        selected = clubs[1]
+      ),
+      
+      selectInput(
+        "position2",
+        "Player 2 - Position",
+        choices = positions,
+        selected = positions[1]
+      ),
+      
+      selectInput("player2",
+                  "Player 2 - Name",
+                  choices = NULL),
+      
+    ),
+    
+    mainPanel(plotOutput("radarMap"))
+    
+  ))
 
 
 # Server logic ----
 server <- function(input, output, session) {
-  observe({
-    old_players <- input$player
-    print(old_players)
-    players <- get_players(input$position, input$club, input$cost)
-    updateSelectInput(session,
-                      "player",
-                      choices = players,
-                      selected = players[1])
+  # player 1 filter
+  player1_filter <- reactive({
+    fpl_data %>% filter(club == input$club1, position == input$position1) %>% arrange(desc(cost))
   })
   
+  observeEvent(player1_filter(), {
+    choices <- unique(player1_filter()$player)
+    updateSelectInput(inputId = "player1", choices = choices)
+  })
   
+  # player 2 filter
+  player2_filter <- reactive({
+    fpl_data %>% filter(club == input$club2, position == input$position2) %>% arrange(desc(cost))
+  })
   
+  observeEvent(player2_filter(), {
+    choices <- unique(player2_filter()$player)
+    updateSelectInput(inputId = "player2", choices = choices)
+  })
   
-  # functions
-  get_players <- function(position_value, club_value, cost_value) {
-    player_data <- fpl_data
-    if (position_value != 'ALL') {
-      player_data <- player_data %>% filter(position == position_value)
-    }
-    if (club_value != 'ALL') {
-      player_data <- player_data %>% filter(club == club_value)
-    }
-    if (cost_value != 'ALL') {
-   #   player_data <- player_data %>% filter(cost >= cost_value & cost < cost_value + 0.5)
-    }
-    players <- player_data %>% arrange(club, player) %>% pull(player)
-    return (players)
-  }
+  # radar data
+  radar_data_filter <- reactive({
+    
+    # organize data
+    req(input$player1)
+    req(input$player2)
+    fpl_data %>% filter(player %in% c(input$player1, input$player2)) %>% select(radarCols)
+  })
+  
+  output$radarMap <- renderPlot({
+    
+    plot_data <- radar_data_filter()
+    plot_data <- rbind(c(250, 40, 40, 3420, 20, 20) , rep(0, 6) , plot_data)
+    
+    # color vector
+    colors_border = c(rgb(0.2, 0.5, 0.5, 0.9),
+                      rgb(0.8, 0.2, 0.5, 0.9) ,
+                      rgb(0.7, 0.5, 0.1, 0.9))
+    
+    colors_in = c(rgb(0.2, 0.5, 0.5, 0.4),
+                  rgb(0.8, 0.2, 0.5, 0.4) ,
+                  rgb(0.7, 0.5, 0.1, 0.4))
+    
+    # plot with default options:
+    radarchart(
+      plot_data,
+      axistype = 1 ,
+      #custom polygon
+      pcol = colors_border ,
+      pfcol = colors_in ,
+      plwd = 4 ,
+      plty = 1,
+      #custom the grid
+      cglcol = "grey",
+      cglty = 1,
+      axislabcol = "grey",
+      caxislabels = seq(0, 20, 5),
+      cglwd = 0.8,
+      #custom labels
+      vlcex = 0.8
+    )
+    
+    # add a legend
+    legend(
+      x = 0.7,
+      y = 1,
+      legend = rownames(plot_data[-c(1, 2),]),
+      bty = "n",
+      pch = 20 ,
+      col = colors_in ,
+      text.col = "grey",
+      cex = 1.2,
+      pt.cex = 3
+    )
+    
+    radarchart( plot_data  , axistype=1 , 
+                pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+                cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
+                vlcex=0.8 
+    )
+    
+  })
   
 }
 
